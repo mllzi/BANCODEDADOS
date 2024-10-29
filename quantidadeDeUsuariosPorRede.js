de23bfff465af98afea8d477f44b876a43bc36c8
bia <bianca.calixto.soares@escola.pr.gov.br>
2024-10-29 11:40:27 -0300

    Update quantidadeDeUsuariosPorRede.js

--- quantidadeDeUsuariosPorRede.js
+++ quantidadeDeUsuariosPorRede.js
@@ -10,7 +10,7 @@ const data = [
     {
         x: nomeDasRedes,
         y: quantidadeUsuariosPorRede,
-        type: 'bar'
+        type: 'bar',
         marker: {
             color: getCSS('--primary-color')
         }
